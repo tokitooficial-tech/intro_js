0fc45bd7abf7a09861f7f102f04a64895f68bd99
tokitooficial-tech <tokitooficial@gmail.com>
2026-04-29 09:19:58 -0300

    feat: javascript externo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// variavel recebe do documento  de um elemento com id
+const botao = document . getElementById ("botao-ok") 
+console.log (botao.textContent)
+
+// variavel 
+// var, let = variavel que muda durante o processo
+// const = variaveis que não podem mudar seu conteúdo
+// esoapço reservado na memoria do computador 
+let idade = 34
+console.log (idade)
+idade = 24
+console.log(idade)
